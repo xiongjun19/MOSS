819533ad9f46198f4341356e3f0fdc3109540004
JunXiong <jun.xiong@lightelligence.ai>
2023-09-14 07:33:56 +0000

    adding lora_model and docker file

---
+++ models/lora_model.py
@@ -0,0 +1,44 @@
+# coding=utf8
+
+
+import os
+import tensorflow as tf
+
+
+'''
+self.conn = redis.Redis(host='localhost', port=6379, db=0)
+'''
+
+class LoRaModel(object):
+    def __init__(self, r, hidden):
+        self.B = tf.Variable(tf.truncated_normal([hidden, r], stddev=0.1,
+            seed=7))
+        self.A = tf.Variable(tf.zeros([r, hidden]))
+
+    def run_forward(self, embed):
+        hid = tf.matmul(embed, self.B)
+        hid = tf.matmul(hid, self.A)
+        return hid
+
+
+def save_model(model_path):
+    r = 8
+    hid_dim = 4096
+    model = LoRaModel(r, hid_dim)
+    input_rnd = tf.placeholder(tf.float32, shape=[None, None, hid_dim])
+    hid_state =model.run_forward(input_rand)
+    init_op = tf.global_variables_initializer()
+    with tf.Session(config=config) as sess:
+        sess.run(init_op)
+        writer = tf.summary.FileWriter('./graphs', graph=sess.graph)
+        tf.saved_model.simple_save(sess,
+                                   model_path,
+                                   inputs = {'embed': input_rnd},
+                                   outputs = {'hid_state': img})
+        writer.flush()
+        writer.close()
+
+
+if __name__ == '__main__':
+    model_path =  'moss_lora'
+    save_model(model_path)
